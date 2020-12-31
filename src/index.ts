27ffe7b5f45ff8306a18195a245f0f443071908c
= <=>
2020-12-31 18:21:17 +0300

    Added morgan logger

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 import authorizedRoute from "./middleware/authorizedRoute";
 import cookieParser from "cookie-parser"
 import authController from "./controllers/authController";
+import morgan from "morgan"
 const app = express();
 
 dotenv.config()
+app.use(morgan("combined"))
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use("/api/auth", authController)
