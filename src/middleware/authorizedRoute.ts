e0785a71d4105a0dda09e588cb4c49dfc2bd4f0f
= <=>
2020-12-31 15:13:42 +0300

    Add  option to middleware

--- src/middleware/authorizedRoute.ts
+++ src/middleware/authorizedRoute.ts
@@ -1,21 +1,22 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export default function authorizedRoute(req: Request, res: Response, next: NextFunction) {
-  const header = req.header("authorization")
-  const token = header?.split("Bearer ")[1] ?? null
-  if (token === null) {
-    return res.sendStatus(401)
-  }
-  jwt.verify(token, process.env.AUTH_TOKEN_SECRET as string, (err, user) => {
-    if (err instanceof jwt.TokenExpiredError) {
-      return res.status(401).send("Token expired")
+export default function authorizedRoute(ignoreExpiredTokens: boolean = false) {
+  return function (req: Request, res: Response, next: NextFunction) {
+    const header = req.header("authorization");
+    const token = header?.split("Bearer ")[1] ?? null;
+    if (token === null) {
+      return res.sendStatus(401);
     }
-    else if (err instanceof jwt.JsonWebTokenError) {
-      return res.status(401).send("Invalid token")
-    }
-    //@ts-ignore
-    req.user = user
-    return next()
-  })
+    jwt.verify(token, process.env.AUTH_TOKEN_SECRET as string, (err, user) => {
+      if (err instanceof jwt.TokenExpiredError && !ignoreExpiredTokens) {
+        return res.status(401).send("Token expired");
+      } else if (err instanceof jwt.JsonWebTokenError) {
+        return res.status(401).send("Invalid token");
+      }
+      //@ts-ignore
+      req.user = user;
+      return next();
+    });
+  };
 }
